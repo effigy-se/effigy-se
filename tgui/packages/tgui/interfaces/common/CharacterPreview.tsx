8f9f159d0b31a84758f0fc321e362bb6751a79c2
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2023-04-29 17:31:05 -0700

    Update CharacterPreview.tsx

--- tgui/packages/tgui/interfaces/common/CharacterPreview.tsx
+++ tgui/packages/tgui/interfaces/common/CharacterPreview.tsx
@@ -1,9 +1,16 @@
 import { ByondUi } from '../../components';
 
-export const CharacterPreview = (props: { height: string; id: string }) => {
+export const CharacterPreview = (props: {
+  width?: string; // EFFIGY EDIT CHANGE - CUSTOMIZATION
+  height: string;
+  id: string;
+}) => {
+  // EFFIGY EDIT CHANGE START - CUSTOMIZATION
+  const { width = '220px' } = props;
+  // EFFIGY EDIT CHANGE END - CUSTOMIZATION
   return (
     <ByondUi
-      width="220px"
+      width={width} // EFFIGY EDIT CHANGE - CUSTOMIZATION
       height={props.height}
       params={{
         id: props.id,
