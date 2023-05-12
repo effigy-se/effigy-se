e648d21e7bbe050e0d973738d4b4ca2f14bd5875
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2023-05-11 19:03:48 -0700

    Update reducer.js

--- tgui/packages/tgui-panel/settings/reducer.js
+++ tgui/packages/tgui-panel/settings/reducer.js
@@ -15,11 +15,11 @@ const initialState = {
   fontSize: 13,
   fontFamily: FONTS[0],
   lineHeight: 1.2,
-  theme: 'light',
+  theme: 'dark',
   adminMusicVolume: 0.5,
   // Keep these two state vars for compatibility with other servers
   highlightText: '',
-  highlightColor: '#ffdd44',
+  highlightColor: '#00ffff',
   // END compatibility state vars
   highlightSettings: [defaultHighlightSetting.id],
   highlightSettingById: {
