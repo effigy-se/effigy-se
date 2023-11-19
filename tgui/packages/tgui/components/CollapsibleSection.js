d5be637a86631e375d4003518fd4e56511b3773b
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2023-11-18 16:01:20 -0800

    Update CollapsibleSection.js

--- tgui/packages/tgui/components/CollapsibleSection.js
+++ tgui/packages/tgui/components/CollapsibleSection.js
@@ -13,7 +13,11 @@ export const CollapsibleSection = (props, context) => {
     showButton = !forceOpen,
     ...rest
   } = props;
-  const [isOpen, setOpen] = useLocalState(context, `open_collapsible_${sectionKey}`, startOpen);
+  const [isOpen, setOpen] = useLocalState(
+    context,
+    `open_collapsible_${sectionKey}`,
+    startOpen
+  );
   return (
     <Section
       fitted={!forceOpen && !isOpen}
