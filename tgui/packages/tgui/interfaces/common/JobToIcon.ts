dd153e649649c9c4032c81b8dcd7f95680022eb9
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2024-04-18 12:52:59 -0700

    Removes virologist job (#82723)
    ## About The Pull Request
    As the title says. Removes the job. Whole wing of medical is still
    there.
    
    ![Screenshot 2024-04-17
    041852](https://github.com/tgstation/tgstation/assets/42397676/b7b55cd6-b6c1-4a0b-97c8-c7d55d4939ed)
    
    (MSO)
    
    ![image](https://github.com/tgstation/tgstation/assets/42397676/3be548f4-9558-4d4d-bc7b-d9cbbbac1d5d)
    
    ## Why It's Good For The Game
    It's boring. It's so boring that when it decides to become unboring it
    just attempts to ruin the fun for everyone else
    ## Changelog
    :cl:
    del: Due to a need for company restructuring, virologists have been laid
    off by CentCom and doctors can optionally take their place.
    /:cl:
    
    ---------
    
    Co-authored-by: MrMelbert <51863163+MrMelbert@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/common/JobToIcon.ts
+++ tgui/packages/tgui/interfaces/common/JobToIcon.ts
@@ -55,6 +55,5 @@ export const JOB2ICON = {
   'Station Engineer': 'gears',
   'Syndicate Operative': 'dragon',
   'Veteran Security Advisor': 'ribbon',
-  Virologist: 'virus',
   Warden: 'handcuffs',
 } as const;
