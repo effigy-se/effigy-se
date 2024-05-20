857c77e5b3fa7a8c1ef42e27d787f92ded0a27f6
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2024-05-20 15:54:51 -0700

    Update face_cursor_combat_mode.tsx

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/face_cursor_combat_mode.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/face_cursor_combat_mode.tsx
@@ -1,13 +1,9 @@
-import { multiline } from 'common/string';
-
 import { CheckboxInput, FeatureToggle } from '../../base';
 
 export const face_cursor_combat_mode: FeatureToggle = {
   name: 'Face cursor with combat mode',
   category: 'GAMEPLAY',
-  description: multiline`
-    When toggled, you will now face towards the cursor
-    with combat mode enabled.
-  `,
+  description:
+    'When toggled, you will now face towards the cursor with combat mode enabled.',
   component: CheckboxInput,
 };
